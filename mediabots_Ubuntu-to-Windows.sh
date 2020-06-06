41f372bfbd7ea8c22dfce3660d0d0ea354ec5311
Yassine Meftah <meftah.champart@gmail.com>
2020-06-06 17:14:00 -0500

    first commit

--- mediabots_Ubuntu-to-Windows.sh
+++ mediabots_Ubuntu-to-Windows.sh
@@ -45,7 +45,7 @@ else
 	# sleep 30
 	# exit 1
 fi
-sudo wget -P /mediabots http://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO
+sudo wget -P /mediabots http://207.180.222.194/en_windows_10_pro_10240_x64_dvd.iso
 sudo wget -P /floppy https://ftp.mozilla.org/pub/firefox/releases/64.0/win32/en-US/Firefox%20Setup%2064.0.exe
 sudo mv /floppy/'Firefox Setup 64.0.exe' /floppy/Firefox.exe
 sudo wget -P /floppy https://downloadmirror.intel.com/23073/eng/PROWinx64.exe # Intel Network Adapter for Windows Server 2012 R2
